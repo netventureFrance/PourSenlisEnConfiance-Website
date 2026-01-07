b72a0131548a39ea481b3292b0e6947e84275af6
Yan Heydlauf <y.heydlauf@netventure.tv>
2026-01-07 03:32:45 +0100

    Remove temporary trigger function

--- netlify/functions/trigger-stats.js
+++
@@ -1,6 +0,0 @@
-// Temporary trigger for testing daily stats email
-const dailyStats = require('./daily-stats.js');
-
-exports.handler = async (event, context) => {
-    return dailyStats.handler(event, context);
-};
